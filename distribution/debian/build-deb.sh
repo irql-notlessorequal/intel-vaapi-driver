d2b8812f9380be173a6ad7fe74084e9190342b2f
Dimitris <darnellos8@yahoo.gr>
2025-08-13 20:04:01 +0200

    Add clarifying output for reverting the driver

--- distribution/debian/build-deb.sh
+++ distribution/debian/build-deb.sh
@@ -27,5 +27,11 @@ cp build/src/i965_drv_video.so distribution/debian/i965-va-driver/usr/lib/x86_64
 cd distribution/debian
 dpkg-deb --build i965-va-driver
 
+echo ''
 echo 'Ubuntu/debian package has been built at distribution/debian/i965-va-driver.deb'
 echo 'Install with: sudo dpkg -i i965-va-driver.deb'
+echo ''
+echo 'To revert back to the regular driver, you can install i965-va-driver-shaders'
+echo 'with: sudo apt install i965-va-driver-shaders - which removes the existing'
+echo 'i965-va-driver. Then run: sudo apt install i965-va-driver - to install the'
+echo 'regular driver from the official repo.'
